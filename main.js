42c588e1b53aa942a2ae0886a9dbd551ac11cf81
durvish2911 <72204035+Durvish2911@users.noreply.github.com>
2021-11-04 12:32:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,18 +4,24 @@ function preload(){
 
 }
 function setup(){
-    canvas=createCanvas(680,780);
+    canvas=createCanvas(480,480);
     canvas.center();
     video=createCapture();
     video.hide();
 }
 function draw(){
-   image(video,0,0,680,780);
+   image(video,0,0,480,480);
    if(war !=""){
        od.dectect(video,gotresult);
        for(i=0;i<array.length;i++){
            document.getElementById("b1").innerHTML="status:-objects detected";
            document.getElementById("b2").innerHTML="status:- no of objects detected are "+array.length;
+           fill("green");
+           var p=floor(array[i].confidence*100);
+           text(array[i].label+" "+p+" % ",array[i].x+15,array[i].y+15);
+           nofill();
+           stroke("green");
+           rect(array[i].x,array[i].y,array[i].width,array[i].height)
        }
    }
 }
@@ -27,7 +33,7 @@ function ml(){
     console.log("model loaded");
     war=true;
 }
-function gr(error,results){
+function gotresult(error,results){
     if(error){
         console.error(error);
     }else{
